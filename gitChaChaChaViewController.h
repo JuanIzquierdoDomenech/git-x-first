7dc9e953f8335ae1d41c313c4805867520b2bd00
Juan Jesus Izquierdo Domenech <juanjesusizquierdodomenech@gmail.com>
2013-01-31 16:30:45 +0100

    My first commit within Xcode to github!!! Miracle?

---
+++ gitChaChaChaViewController.h
@@ -0,0 +1,13 @@
+//
+//  gitChaChaChaViewController.h
+//  Git-x-first
+//
+//  Created by Juan Jesús Izquierdo Doménech on 31/01/13.
+//  Copyright (c) 2013 Juan Jesús Izquierdo Doménech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface gitChaChaChaViewController : UIViewController
+
+@end
---
+++ gitChaChaChaViewController.m
@@ -0,0 +1,38 @@
+//
+//  gitChaChaChaViewController.m
+//  Git-x-first
+//
+//  Created by Juan Jesús Izquierdo Doménech on 31/01/13.
+//  Copyright (c) 2013 Juan Jesús Izquierdo Doménech. All rights reserved.
+//
+
+#import "gitChaChaChaViewController.h"
+
+@interface gitChaChaChaViewController ()
+
+@end
+
+@implementation gitChaChaChaViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
