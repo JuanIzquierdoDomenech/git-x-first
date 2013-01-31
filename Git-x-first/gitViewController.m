640a126022847a1edbf9a8ecb842863ca17e2cb3
Juan Jesus Izquierdo Domenech <juanjesusizquierdodomenech@gmail.com>
2013-01-31 16:24:25 +0100

    Initial Commit

---
+++ Git-x-first/gitViewController.m
@@ -0,0 +1,29 @@
+//
+//  gitViewController.m
+//  Git-x-first
+//
+//  Created by Juan Jesús Izquierdo Doménech on 31/01/13.
+//  Copyright (c) 2013 Juan Jesús Izquierdo Doménech. All rights reserved.
+//
+
+#import "gitViewController.h"
+
+@interface gitViewController ()
+
+@end
+
+@implementation gitViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Git-x-first/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Git-x-first
+//
+//  Created by Juan Jesús Izquierdo Doménech on 31/01/13.
+//  Copyright (c) 2013 Juan Jesús Izquierdo Doménech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "gitAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([gitAppDelegate class]));
+    }
+}
